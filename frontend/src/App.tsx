6428d55c0d18a0950bb1ad1342724d73041de215
Shahadat-Hossen-008 <penaxavier078@gmail.com>
2026-01-10 20:06:11 +0600

    Config file firebase auth and shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,14 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
-    <div className="text-center m-8 text-cyan-600 text-5xl">Hello, World</div>
+    <>
+      <div className="text-center m-8 text-green-600 text-5xl">Hello, do you need help?</div>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+         <Button>Click me</Button>
+      </div>
+    </>
+    
   );
 };
 
